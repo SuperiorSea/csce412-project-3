82e0f513fed80e1b5852ac536939ec8c3bec4e64
Dylan Marintzer <dylanmarintzer@tamu.edu>
2026-02-24 15:34:26 -0600

    adding random request generator to load balancer

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,13 +1,18 @@
 #include "LoadBalancer.h"
 #include "IPAddress.h"
 #include <iostream>
+#include <random>
 
 // constructor
-LoadBalancer::LoadBalancer(int num_servers, int num_wait_clock_cycles, int min_request_time, int max_request_time)
- : last_scale_clock_cycle(0), num_wait_clock_cycles(num_wait_clock_cycles), min_request_time(min_request_time), max_request_time(max_request_time) {
+LoadBalancer::LoadBalancer(int num_servers, int total_clock_cycles, int num_wait_clock_cycles, int min_request_time, int max_request_time)
+ : num_servers(num_servers), total_clock_cycles(total_clock_cycles), last_scale_clock_cycle(0), num_wait_clock_cycles(num_wait_clock_cycles), min_request_time(min_request_time), max_request_time(max_request_time) {
     for (int i = 0; i < num_servers; i++) {
         addServer();
     }
+
+    // shouldn't hardcode these, but for testing
+    min_queue_size_for_scaling = 50 * this->num_servers;
+    max_queue_size_for_scaling = 80 * this->num_servers;
 }
 
 // helpers
@@ -23,4 +28,32 @@ void LoadBalancer::removeServer() {
         servers.pop_back();
         std::cout << "[WEBSERVER ACTION] Removed server with ID: " << removed_id << " | Total servers: " << servers.size() << "\n";
     }
+}
+
+// make random request
+Request LoadBalancer::makeRandomRequest() {
+    // static RNG so consecutive calls produce different values
+    static std::mt19937 rng(std::random_device{}());
+
+    // random distributions
+    std::uniform_int_distribution<int> ip_dist(0, 255);
+    std::uniform_int_distribution<int> time_dist(min_request_time, max_request_time);
+    std::uniform_int_distribution<int> job_dist(0, 1); // 0 for 'P', 1 for 'S'
+
+    // lambda to generate random IP address string
+    auto random_ip = [&]() {
+        return std::to_string(ip_dist(rng)) + "." +
+               std::to_string(ip_dist(rng)) + "." +
+               std::to_string(ip_dist(rng)) + "." +
+               std::to_string(ip_dist(rng));
+    };
+
+    // create request with random values
+    IPAddress in(random_ip());
+    IPAddress out(random_ip());
+    int time = time_dist(rng);
+    char job = (job_dist(rng) == 0) ? 'P' : 'S';
+
+    // return request
+    return Request(in, out, time, job);
 }
--- LoadBalancer.h
+++ LoadBalancer.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <WebServer.h>
-#include <RequestQueue.h>
+#include "WebServer.h"
+#include "RequestQueue.h"
 #include <vector>
 
 class LoadBalancer {
@@ -8,6 +8,10 @@ class LoadBalancer {
         // for the servers and queue
         std::vector<WebServer> servers;
         RequestQueue request_queue;
+
+        // for initialization
+        int num_servers;
+        int total_clock_cycles;
 
         // for scaling, track the last clock cycle we scaled, and how many cycles we have to wait until scaling again
         int last_scale_clock_cycle;
@@ -17,13 +21,18 @@ class LoadBalancer {
         int min_request_time;
         int max_request_time;
 
+        // for scaling
+        int min_queue_size_for_scaling;
+        int max_queue_size_for_scaling;
+
         // helpers
         void addServer();
         void removeServer();
 
-        // make random request
-        Request makeRandomRequest();
+        // make random request (public so it can be tested)
     public:
+        Request makeRandomRequest();
+
         // constructor
-        LoadBalancer(int num_servers, int num_wait_clock_cycles, int min_request_time, int max_request_time);
-}
+        LoadBalancer(int num_servers, int total_clock_cycles, int num_wait_clock_cycles, int min_request_time, int max_request_time);
+};
